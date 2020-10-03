c5e3c76786ddf9a769d19f723d119ce6dcf6ba7d
Tafadzwa <tafamanza11@gmail.com>
2020-10-03 16:31:03 +0200

    Updates

---
+++ precache-manifest.f68ad8b3e920fcf89399190167f4a097.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fa3abd091926b8d287faa26faef1a9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "59c9e45233a00be0b0fa",
+    "url": "/portfolio/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "ed971755e8176d06838a",
+    "url": "/portfolio/static/css/main.4425e540.chunk.css"
+  },
+  {
+    "revision": "59c9e45233a00be0b0fa",
+    "url": "/portfolio/static/js/2.238cd120.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/portfolio/static/js/2.238cd120.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed971755e8176d06838a",
+    "url": "/portfolio/static/js/main.bd011a2a.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "178714f120b14c6e20401f7e9032f40e",
+    "url": "/portfolio/static/media/bev.178714f1.png"
+  },
+  {
+    "revision": "fcacfde38e5ed11169ccc2af89b8524c",
+    "url": "/portfolio/static/media/budget.fcacfde3.png"
+  },
+  {
+    "revision": "0e67b5f81b9627e2f5b8dc25d2883222",
+    "url": "/portfolio/static/media/calculator.0e67b5f8.png"
+  },
+  {
+    "revision": "f6ed2cb45a943e8e0b569ce975273606",
+    "url": "/portfolio/static/media/clothing.f6ed2cb4.PNG"
+  },
+  {
+    "revision": "a49c1aae4467d6188010909c85f5c685",
+    "url": "/portfolio/static/media/dice.a49c1aae.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/portfolio/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a3891b14c299d385e04991a5fe1da4dc",
+    "url": "/portfolio/static/media/forkify.a3891b14.png"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/portfolio/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "a3d49069e134ed8bea9f3d1e12e0312b",
+    "url": "/portfolio/static/media/male.a3d49069.png"
+  },
+  {
+    "revision": "a3d49069e134ed8bea9f3d1e12e0312b",
+    "url": "/portfolio/static/media/male1.a3d49069.png"
+  },
+  {
+    "revision": "a80a31dea1f1988fa43932b96f355dee",
+    "url": "/portfolio/static/media/notes.a80a31de.png"
+  },
+  {
+    "revision": "7f89edd9301eb167b51c061e5045c84f",
+    "url": "/portfolio/static/media/portfolio.7f89edd9.PNG"
+  },
+  {
+    "revision": "b496022fbf645c4a0f82a2a39cb24582",
+    "url": "/portfolio/static/media/responsive.b496022f.png"
+  },
+  {
+    "revision": "9b3796fff1414570f2fcef4c44830691",
+    "url": "/portfolio/static/media/studymate.9b3796ff.PNG"
+  },
+  {
+    "revision": "d567cb71dbb2d4b1cf21f393c6434c50",
+    "url": "/portfolio/static/media/weather.d567cb71.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.2bd865629f36809bf45fbedc4ccf1b4e.js"
+  "/portfolio/precache-manifest.f68ad8b3e920fcf89399190167f4a097.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
